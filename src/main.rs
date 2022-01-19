640de002d54955f900d31e0203ac0acf32a8ec4e
anismousse <anismousse@gmail.com>
2022-01-18 23:06:30 -0800

    Setup the main function to create an alternate Terminal screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
+use crossterm::{
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    cursor::{Hide, Show},
+    ExecutableCommand
+};
+
+use std::{
+    error::Error,
+    io,
+    // thread
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    //Closing and cleaning up terminal
     println!("Hello, world!");
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+    Ok(())
 }
